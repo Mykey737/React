4d916c9380dd0c074c8fc4657d502b157740cb80
Mike Pena <mykey737@gmail.com>
2021-04-27 00:28:54 -0400

    React Redux Form

--- nucampsite/src/components/CampsiteInfoComponent.js
+++ nucampsite/src/components/CampsiteInfoComponent.js
@@ -1,10 +1,10 @@
-import React from "react";
-import { Card, CardImg, CardText, CardBody, Breadcrumb, BreadcrumbItem } from "reactstrap";
+import React, { Component } from "react";
+import { Card, CardImg, CardText, CardBody, Breadcrumb, BreadcrumbItem, Button, Modal, ModalHeader, ModalBody, Form, FormGroup, Input, Label } from "reactstrap";
 import { Link } from "react-router-dom";
 
 function RenderCampsite({ campsite }) {
   return (
-    <div className="col-md-5 and m-1">
+    <div className="col-md-11 m-1">
       <Card>
         <CardImg top src={campsite.image} alt={campsite.name} />
         <CardBody>
@@ -15,10 +15,72 @@ function RenderCampsite({ campsite }) {
   );
 }
 
+class CommentForm extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isModalOpen: false
+    };
+
+    this.toggleModal = this.toggleModal.bind(this);
+    this.handleComment = this.handleComment.bind(this);
+
+  }
+
+  toggleModal() {
+    this.setState({
+      isModalOpen: !this.state.isModalOpen,
+    });
+  }
+
+  handleComment(event) {
+    alert(`Username: ${this.username.value}, Password: ${this.password.value}, Remember Me: ${this.remember.checked}`);
+    this.toggleModal();
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+        
+        <span className="col-md-5 m-1">
+          <Button outline onClick={this.toggleModal}>
+            <i className="fa fa-pencil fa-lg" /> Submit Comment
+          </Button>
+        </span>
+
+        <Modal isOpen={this.state.isModalOpen} toggle={this.toggleModal}>
+          <ModalHeader toggle={this.toggleModal}></ModalHeader>
+          <ModalBody>
+            <Form onSubmit={this.handleLogin}>
+              <FormGroup>
+                <Label htmlFor="username">Username</Label>
+                <Input type="text" id="username" name="username" innerRef={input =>this.username = input} />
+              </FormGroup>
+              <FormGroup>
+                <Label htmlFor="password">Password</Label>
+                <Input type="password" id="password" name="password" innerRef={input =>this.password = input} />
+              </FormGroup>
+              <FormGroup check>
+                <Label check>
+                <Input type="checkbox" name="remember" innerRef={input => this.remember = input} />
+                Remember me
+                </Label>
+              </FormGroup>
+              <Button type="submit" value="submit" color="primary">Login</Button>
+            </Form>
+          </ModalBody>
+        </Modal>
+      </React.Fragment>
+    )
+  };
+}
+
 function RenderComments({ comments }) {
   if (comments) {
     return (
-      <div className="col-md-5 m-1">
+      <div className="col-md m-1">
         <h4>Comments</h4>
         {comments.map((comment) => {
           return (
@@ -57,13 +119,18 @@ function CampsiteInfo(props) {
           </div>
         </div>
         <div className="row">
-          <RenderCampsite campsite={props.campsite} />
-          <RenderComments comments={props.comments} />
+          <div className="col-6">
+            <RenderCampsite campsite={props.campsite} />
+          </div>
+          <div className="col-6">
+            <RenderComments comments={props.comments} />
+            <CommentForm />
+          </div>
         </div>
       </div>
     );
   }
-  return <div />;
+  return <div />; 
 }
 
 export default CampsiteInfo;
